143ea40db5f69df63797a64f2f1f40cc4f3b3119
thedanielju <danjuacademic@gmail.com>
2025-02-28 22:33:41 -0500

    added TypeA as a subclass of Thing. contains maybeturn which directs right, left, or straight. Adjusted Things to be an abstract class with an abstract method for maybeturns, which will likely be recycled later

--- TypeA.java
+++ TypeA.java
@@ -1,3 +1,9 @@
-public class TypeA {
-    
+import java.util.Random;
+
+public class TypeA extends Thing {
+    public static Random rand = new Random(System.currentTimeMillis());
+
+    public TypeA(int row, int col) {
+        super(row, col, 'r');
+    }
 }
