6e18bd0b1d8cf2680a26c6264b0d6d64d11bfdf7
thedanielju <danjuacademic@gmail.com>
2025-02-28 22:37:40 -0500

    added TypeB subclass of Thing, with timeSinceLast counter and same turn commands as TypeA

--- TypeB.java
+++ TypeB.java
@@ -1,3 +1,31 @@
-public class TypeB {
-    
+import java.util.Random;
+
+public class TypeB extends Thing {
+    public static Random rand = new Random(System.currentTimeMillis());
+    private int timeSinceLast;
+
+    public TypeB(int row, int col) {
+        super(row, col, 'r');
+        this.timeSinceLast = 0;
+    }
+
+    @Override
+    public void maybeTurn() {
+        timeSinceLast++;
+        if (timeSinceLast == 10) {
+            timeSinceLast = 0;
+            int i = rand.nextInt(3);
+            if (i == 1) rightTurn();
+            if (i == 2) leftTurn();
+        }
+    }
+
+    private void rightTurn() {
+        setDir((getDir() +1) % 4);
+    }
+
+    private void leftTurn() {
+       setDir((getDir() + 3) % 4);
+    }   
+
 }
